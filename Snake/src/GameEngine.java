00ba6d9ce5345c0d6923efe649eba0c106144e2d
l30n95 <leon.hennessy@student.ittralee.ie>
2018-11-25 15:39:15 +0000

    John Brosnan helped me with the collision detection of snake going into itself and also figured out what was wrong with the flickering of the main menu.

--- Snake/src/GameEngine.java
+++ Snake/src/GameEngine.java
@@ -9,9 +9,8 @@ public class GameEngine {
     BoardGUI board = new BoardGUI();
 
 /*Current issues & to-do
--Snake needs to stop when the snakes head hits the body. - Collision detection
 -Must implement a way to increase the game score and display it to the screen.
--Must create a main menu with music in the background. -- current issue(Menu keeps flickering)
+-Must add music to the main menu.
 -Must put sound effect when snake eats food.
 */    }
 }
